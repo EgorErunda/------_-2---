edfb5cdacb10a153ee378b20741a894c010c4b02
ywezj <ywezj@icloud.com>
2025-06-25 03:33:15 +0300

    modified:   database.py

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
 from peewee import *
+from datetime import datetime
+import pytz
+from config import TIMEZONE
 
 db = SqliteDatabase('calendar.db')
 
@@ -8,17 +11,17 @@ class BaseModel(Model):
 
 class User(BaseModel):
     user_id = IntegerField(unique=True)
-    timezone = CharField(default='UTC')
+    created_at = DateTimeField(default=datetime.now(pytz.timezone(TIMEZONE)))
 
 class Event(BaseModel):
     user = ForeignKeyField(User, backref='events')
-    name = TextField()
+    name = CharField(max_length=255)
     date = DateField()
     time = TimeField()
-    reminder_time = IntegerField()  # за сколько минут напоминать
+    reminder_minutes = IntegerField(default=15)
+    created_at = DateTimeField(default=datetime.now(pytz.timezone(TIMEZONE)))
     is_completed = BooleanField(default=False)
 
 def initialize_db():
-    db.connect()
-    db.create_tables([User, Event], safe=True)
-    db.close()
+    with db:
+        db.create_tables([User, Event])
